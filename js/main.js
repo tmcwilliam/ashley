2dad00afaf7633ba2e22a07f61d29e8cc05a6fed
Travis McWilliam <travis@sproutsocial.com>
2014-07-23 20:51:42 -0500

    videolightbox

--- js/main.js
+++ js/main.js
@@ -1,41 +1,148 @@
-$(document).ready(function($){
-	$('.lightbox').lightbox();
+var ASHLEY_PHOTOS = [
+		{
+			"image_url":"images/00_Ashley-logo.jpg",
+			"href":"index.html",
+			"video":"index.html",
+			"target": "_self"
+		},{
+			"image_url":"images/01-Modern-Brown-Sofa.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/01-Modern-Brown-Sofa-Fb.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720&image=images/01-Modern-Brown-Sofa.jpg",
+			"video": "vdbplayer.swf?volume=100&url=videos/01-Modern-Brown-Sofa-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/02A-B582-Sleek-Brown-Bedroom.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/02A-B582-Sleek-Brown-Bedroom-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/02A-B582-Sleek-Brown-Bedroom-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/03-Moody-Dining.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/03-Moody-Dining-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/03-Moody-Dining-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/04-Uph-Blue-Sofa.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/04-Uph-Blue-Sofa-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/04-Uph-Blue-Sofa-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/05-Corson-Mocha-Sofa.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/05-Corson-Mocha-Sofa-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/05-Corson-Mocha-Sofa-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/06-Large-French-Country-Table.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/06-Large-French-Country-Table-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/06-Large-French-Country-Table-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/08-Coutmeyers-Amber-Stationary.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/08-Coutmeyers-Amber-Stationary-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/08-Coutmeyers-Amber-Stationary-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/09-Arrieta.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/09-Arrieta-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/09-Arrieta-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/10-Navosta-Charc.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/10-Navosta-Charc-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/10-Navosta-Charc-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/11-Kaylor-Taupe.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/11-Kaylor-Taupe-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/11-Kaylor-Taupe-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/12-Kaylor-Quarry.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/12-Kaylor-Quarry-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/12-Kaylor-Quarry-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/13-Kaylor-Orange.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/13-Kaylor-Orange-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/13-Kaylor-Orange-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/14-Ashley-876-Orrin-Sand.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/14-Ashley-876-Orrin-Sand-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/14-Ashley-876-Orrin-Sand-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/15-Corson-Copper-Sofa.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/15-Corson-Copper-Sofa-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/15-Corson-Copper-Sofa-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/16-Corson-Green-Sofa.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/16-Corson-Green-Sofa-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/16-Corson-Green-Sofa-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/17-Modern-Dining-Table-Not-Set_1.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/17-Modern-Dining-Table-Not-Set-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/17-Modern-Dining-Table-Not-Set-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/18-Combo-Living-Dining.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/18-Combo-Living-Dining-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/18-Combo-Living-Dining-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/19-Studio-Product-Shots-SPRING.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/19-Studio-Product-Shots-SPRING-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/19-Studio-Product-Shots-SPRING-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/20-Studio-Product-Shots-GENERIC.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/20-Studio-Product-Shots-GENERIC-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/20-Studio-Product-Shots-GENERIC-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/21-Studio-Product-Shots-FALL.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/21-Studio-Product-Shots-FALL-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/21-Studio-Product-Shots-FALL-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/22-SPS-SPRING_1.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/22-SPS-SPRING-F.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/22-SPS-SPRING-F.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/23-SPS-Generic-Brown-Sleigh-Bed.jpg",
+			"href": "http://www.ashley.pulsestudio.com/videos/23-SPS-Generic-Brown-Sleigh-Bed-F-A.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/23-SPS-Generic-Brown-Sleigh-Bed-F-A.mp4",
+			"class": "voverlay"
+		},{
+			"image_url":"images/24-Mixed-Set-Window-Bed_1.jpg",
+			"href":"http://www.ashley.pulsestudio.com/videos/24-Mixed-Set-Window-Bed-F-A.mp4?lightbox[force]=jwplayer&lightbox[width]=1280&lightbox[height]=720",
+			"video": "vdbplayer.swf?volume=100&url=videos/24-Mixed-Set-Window-Bed-F-A.mp4",
+			"class": "voverlay"
+		}
+	];
 
-	var $photos = $("#ashley-photo-wall");
+$(function(){
 
-	$.get('/photos.json', function(data) {
-		var list = '<% _.forEach(photos, function(photo) { %>' +
+	var list = '<% _.forEach(photos, function(photo) { %>' +
 					'<li data-thumb="<%- photo.image_url %>">' +
-						'<a href="<%- photo.href %>" class="lightbox" target="<%- photo.target %>"></a>' +
+						'<a href="<%- photo.video %>" target="<%- photo.target %>" class="<%- photo.class %>"></a>' +
 					'</li>' +
-					'<% }); %>';
+				'<% }); %>';
 
-		$('#photo-grid').html(_.template(list, { 'photos': data.photos }));
+	$('#photo-grid').html(_.template(list, { 'photos': ASHLEY_PHOTOS }));
 
-		$photos.magicWall({
-			maxItemWidth: 384,
-			maxItemHeight: 270,
-			delay: 1000,
-			pauseOnHover: true,
-			preloadBeforeSwitch: true
-		});
-
-		$photos.magicWall("stop");
-
-		_.forEach(_.range(20), function(idx){
-			setTimeout(function(){
-				$photos.magicWall("switchItem", idx, '-flipX', 1000, "easeInOutCubic");
-			}, idx*200);
-		});
+	$("#ashley-photo-wall").magicWall({
+		maxItemWidth: 384,
+		maxItemHeight: 270,
+		delay: 1000,
+		pauseOnHover: true
 	});
 
-	$(".colorbox").colorbox({
-		onOpen: function(){
-			$("#ashley-photo-wall").magicWall("stop");
-		},
-		
-		onClosed: function(){
-			$("#ashley-photo-wall").magicWall("start");
-		}
+	videoLightBox(".voverlay", {
+		opacity: 0.7,
+		color: "#000000",
+		closeOnComplete: true,
+		volume: 100
 	});
 });
